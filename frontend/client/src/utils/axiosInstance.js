2e78850c371a02b5014a05a4f3b383ca168467d0
DivyasimhaReddy <118110401+DivyasimhaReddy@users.noreply.github.com>
2025-07-18 14:14:58 +0530

    Update axiosInstance.js

--- frontend/client/src/utils/axiosInstance.js
+++ frontend/client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://viva-lpu-3.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
